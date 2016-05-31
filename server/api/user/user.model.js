254b62de4b0464df6c39a1603041398c934d6b30
Adam Keenan <adam.g.keenan@gmail.com>
2016-05-31 01:43:34 -0500

    Added created_at to highlights

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -52,6 +52,11 @@ var UserSchema = new Schema({
     name: {
       type: String,
       required: true
+    },
+    created_at: {
+      type: Date,
+      required: true,
+      default: Date.now
     }
   }]
 }, { toJSON: { virtuals: true } });
