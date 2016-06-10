5744c408dea97be9fcb86d2448e7de7f6ab0f199
Adam Keenan <adam.g.keenan@gmail.com>
2016-06-09 21:28:48 -0500

    Copy jwplayer files on build

--- Gruntfile.js
+++ Gruntfile.js
@@ -414,7 +414,7 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
-            'assets/flowplayer/**/*',
+            'assets/jwplayer*/**/*',
             'index.html'
           ]
         }, {
--- rebuild_prod.fish
+++ rebuild_prod.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/fish
 
-git fetch; and git co origin/master; and grunt build; and forever restart dist/server
+git fetch; and git co origin/master; and bower install; and npm install; and grunt build; and forever restart dist/server
