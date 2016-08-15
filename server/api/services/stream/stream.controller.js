a8a8d92a58b3d6db1ff8b992293cac97da953134
Adam Keenan <adam.keenan@myfarms.com>
2016-08-14 20:55:19 -0400

    Correct silent :P

--- server/api/services/stream/stream.controller.js
+++ server/api/services/stream/stream.controller.js
@@ -5,7 +5,7 @@ exports.status = function (socketio) {
   return function (req, res) {
     if (req.body.name === 'movienight') {
       if (req.body.call === 'publish') {
-        if (!req.body.silent) {
+        if (typeof silent === "undefined" || silent === null) {
           Slack.sendMessage(config.slack.webhook, {
             channel: '#random',
             text: (req.body.who? req.body.who : 'Someone') +  ' started streaming *' + (req.body.title ? req.body.title : 'something') + '*!\nCome join the party: https://yoc.adam-keenan.com/stream'
