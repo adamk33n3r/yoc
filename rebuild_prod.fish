5b83e2362f42d246e42b8690c99fa3e84337b1f6
Adam Keenan <adam.keenan@myfarms.com>
2017-01-06 10:21:45 -0500

    Switch to yarn

--- rebuild_prod.fish
+++ rebuild_prod.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/fish
 
-git fetch; and git co origin/master; and bower install; and npm install; and grunt build; and sudo systemctl restart yoc
+git fetch; and git co origin/master; and bower install; and yarn install; and grunt build; and sudo systemctl restart yoc
