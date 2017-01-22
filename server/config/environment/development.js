fa2dcf80046d31261c724aa23d467091f75b2f26
Adam Keenan <adam.keenan@myfarms.com>
2017-01-22 16:59:33 -0500

    Changed mongo port

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/yoc-dev'
+      uri: 'mongodb://localhost:27272/yoc-dev'
   },
   sequelize: {
     uri: 'sqlite://',
